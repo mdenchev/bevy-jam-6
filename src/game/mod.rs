ce15a61b79ee6979635bf3fd538a0a6f068eaaea
bstriker <strikeforcezero@gmail.com>
2025-05-28 15:00:54 -0400

    chore: conditionally enable bevy_frame_count_log_prefix

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,15 +1,28 @@
 mod rng;
 
 use crate::game::rng::RngPlugin;
+use bevy::app::PluginGroupBuilder;
+use bevy::log::LogPlugin;
 use bevy::prelude::*;
 use bevy_auto_plugin::auto_plugin::*;
+#[cfg(feature = "dev_frame_count_log")]
+use bevy_frame_count_log_prefix::prelude::FrameCountLogPrefixPlugin;
 
 pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     #[auto_plugin(app=app)]
     fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultPlugins);
+        app.add_plugins(default_plugins());
+        #[cfg(feature = "dev_frame_count_log")]
+        app.add_plugins(FrameCountLogPrefixPlugin);
         app.add_plugins(RngPlugin);
     }
+}
+
+fn default_plugins() -> PluginGroupBuilder {
+    let default_plugins = DefaultPlugins.build();
+    #[cfg(feature = "dev_frame_count_log")]
+    let default_plugins = default_plugins.disable::<LogPlugin>();
+    default_plugins
 }
