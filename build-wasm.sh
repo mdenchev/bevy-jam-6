84fe6dd7ff6b4fe84a374dbe317dbe61e34af129
bstriker <strikeforcezero@gmail.com>
2025-05-28 13:55:59 -0400

    chore(build): add wasm build script

---
+++ build-wasm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# cargo install --locked --git https://github.com/TheBevyFlock/bevy_cli bevy_cli
+# env -u RUSTFLAGS cargo install wasm-bindgen-cli --version 0.2.100
+RUSTFLAGS='--cfg getrandom_backend="wasm_js"' bevy build --release --no-default-features true --features wasm --yes web --bundle
+  
