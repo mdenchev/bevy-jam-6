43896fc42ae969d7534778b4a9986724daf77cff
bstriker <strikeforcezero@gmail.com>
2025-05-29 12:40:47 -0400

    chore(demo): animate light

--- src/game/demo/level.rs
+++ src/game/demo/level.rs
@@ -1,6 +1,6 @@
 //! Spawn the main level.
 
-use avian3d::prelude::Collider;
+use avian3d::prelude::{Collider, DistanceJoint, ExternalImpulse, GravityScale, Joint, RigidBody};
 use bevy::color;
 use bevy::prelude::*;
 use bevy_auto_plugin::auto_plugin::*;
@@ -44,17 +44,21 @@ pub fn spawn_level(
 ) {
     let demo_ob1_mesh = Sphere::new(100.0);
     let demo_ob1_collider = Collider::sphere(demo_ob1_mesh.radius);
-    let demo_obj1 = (
-        DemoObj,
-        demo_ob1_collider,
-        Mesh3d(meshes.add(demo_ob1_mesh)),
-        MeshMaterial3d(materials.add(StandardMaterial {
-            base_color: Color::from(color::palettes::css::RED),
-            perceptual_roughness: 0.1,
-            ..Default::default()
-        })),
-        Transform::from_translation(Vec3::NEG_Z * 300.0),
-    );
+    let demo_ob1_transform = Transform::from_translation(Vec3::NEG_Z * 300.0);
+    let demo_obj1 = commands
+        .spawn((
+            DemoObj,
+            RigidBody::Static,
+            demo_ob1_collider,
+            Mesh3d(meshes.add(demo_ob1_mesh)),
+            MeshMaterial3d(materials.add(StandardMaterial {
+                base_color: Color::from(color::palettes::css::RED),
+                perceptual_roughness: 0.1,
+                ..Default::default()
+            })),
+            demo_ob1_transform,
+        ))
+        .id();
 
     let x_len = 500.0;
     let y_len = 5.0;
@@ -63,6 +67,7 @@ pub fn spawn_level(
     let demo_ob2_collider = Collider::cuboid(x_len, y_len, z_len);
     let demo_obj2 = (
         DemoObj,
+        RigidBody::Static,
         demo_ob2_collider,
         Mesh3d(meshes.add(demo_ob2_mesh)),
         MeshMaterial3d(materials.add(StandardMaterial {
@@ -75,42 +80,54 @@ pub fn spawn_level(
 
     let light_mesh = Sphere::new(5.0);
     let light_collider = Collider::sphere(light_mesh.radius);
-    let light = (
-        LightMesh,
-        light_collider,
-        PointLight {
-            intensity: 10000000.0,
-            range: 1000.0,
-            radius: 100.0,
-            shadows_enabled: true,
-            ..Default::default()
-        },
-        Transform::from_translation(Vec3::new(50.0, 50.0, -180.0)),
-        children![(
-            Mesh3d(meshes.add(light_mesh)),
-            MeshMaterial3d(materials.add(StandardMaterial {
-                emissive: Color::WHITE.to_linear(),
-                unlit: true,
+    let light_transform = Transform::from_translation(Vec3::new(50.0, 50.0, -180.0));
+    let light = commands
+        .spawn((
+            LightMesh,
+            RigidBody::Dynamic,
+            ExternalImpulse::new(Vec3::new(6000.0, 150.0, 3000.0)).with_persistence(true),
+            GravityScale(0.0),
+            light_collider,
+            PointLight {
+                intensity: 10000000.0,
+                range: 1000.0,
+                radius: 100.0,
+                shadows_enabled: true,
                 ..Default::default()
-            })),
-        )],
-    );
+            },
+            light_transform,
+            children![(
+                Mesh3d(meshes.add(light_mesh)),
+                MeshMaterial3d(materials.add(StandardMaterial {
+                    emissive: Color::WHITE.to_linear(),
+                    unlit: true,
+                    ..Default::default()
+                })),
+            )],
+        ))
+        .id();
 
-    commands.spawn((
-        Name::new("Level"),
-        Transform::default(),
-        Visibility::default(),
-        StateScoped(Screen::Gameplay),
-        children![
-            (
-                Name::new("Gameplay Music"),
-                music(level_assets.music.clone())
-            ),
-            demo_obj1,
-            demo_obj2,
-            light,
-        ],
-    ));
+    let distance_min_max = (demo_ob1_transform.translation - light_transform.translation).length();
+
+    commands
+        .spawn((
+            Name::new("Level"),
+            Transform::default(),
+            Visibility::default(),
+            StateScoped(Screen::Gameplay),
+            children![
+                (
+                    Name::new("Gameplay Music"),
+                    music(level_assets.music.clone())
+                ),
+                demo_obj2
+            ],
+        ))
+        .add_child(light)
+        .add_child(demo_obj1)
+        .with_child(
+            DistanceJoint::new(demo_obj1, light).with_limits(distance_min_max, distance_min_max),
+        );
 }
 
 #[auto_plugin(app=app)]
