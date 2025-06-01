521b133b2f3ff0aa21625544c33496361bc21fd6
bstriker <strikeforcezero@gmail.com>
2025-06-01 15:03:48 -0400

    refactor: use standard add_plugins

--- src/game/tower_defense/mod.rs
+++ src/game/tower_defense/mod.rs
@@ -7,7 +7,7 @@ use bevy_auto_plugin::auto_plugin::*;
 
 #[auto_plugin(app=app)]
 pub(crate) fn plugin(app: &mut App) {
-    tower::plugin(app);
-    wizard::plugin(app);
-    level::plugin(app);
+    app.add_plugins(tower::plugin);
+    app.add_plugins(wizard::plugin);
+    app.add_plugins(level::plugin);
 }
