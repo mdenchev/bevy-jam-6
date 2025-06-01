24e3698f2e54c849a7c94ae003fe4bbe155f8bd9
Michail Denchev <mdenchev@gmail.com>
2025-06-01 15:28:07 -0400

    Reduct splash time, adjust camera and light values

--- src/game/tower_defense/level.rs
+++ src/game/tower_defense/level.rs
@@ -5,7 +5,6 @@ use crate::game::tower_defense::tower::Tower;
 use crate::game::tower_defense::wizard::Wizard;
 use bevy::color::palettes::css::GREEN;
 use bevy::prelude::*;
-use bevy::ui::OverflowAxis::Visible;
 use bevy_auto_plugin::auto_plugin::*;
 
 #[auto_plugin(app=app)]
@@ -24,15 +23,6 @@ pub fn spawn_level(
         children![
             (Tower, Transform::from_xyz(0.0, 50.0, 0.0)),
             (
-                Mesh3d(meshes.add(Cuboid::new(1000.0, 10.0, 1000.0))),
-                MeshMaterial3d(materials.add(StandardMaterial {
-                    base_color: Color::from(GREEN),
-                    perceptual_roughness: 1.0,
-                    reflectance: 0.0,
-                    ..Default::default()
-                })),
-            ),
-            (
                 Name::new("Grass"),
                 Mesh3d(meshes.add(Cuboid::new(1000.0, 10.0, 1000.0))),
                 MeshMaterial3d(materials.add(StandardMaterial {
@@ -50,6 +40,16 @@ pub fn spawn_level(
             (
                 LightningBall,
                 Transform::from_xyz(0.0, 3.1 * 10.0 + 100.0, 0.8 * 10.0),
+            ),
+            (
+                PointLight {
+                    intensity: 19999999999.0,
+                    range: 100000.0,
+                    radius: 999.0,
+                    shadows_enabled: true,
+                    ..Default::default()
+                },
+                Transform::from_xyz(30.0, 300.0, 80.0),
             )
         ],
     ));
