2a0ac2d2ada38ee7174379defb006728295b779f
Michail Denchev <mdenchev@gmail.com>
2025-06-02 14:10:26 -0400

    feat: Add Spawners

--- src/game/prefabs/mod.rs
+++ src/game/prefabs/mod.rs
@@ -1,4 +1,5 @@
 pub mod enemy;
+pub mod spawner;
 pub mod tower;
 pub mod wizard;
 
@@ -8,6 +9,7 @@ use bevy_auto_plugin::auto_plugin::*;
 #[auto_plugin(app=app)]
 pub(crate) fn plugin(app: &mut App) {
     app.add_plugins(enemy::plugin);
+    app.add_plugins(spawner::plugin);
     app.add_plugins(tower::plugin);
     app.add_plugins(wizard::plugin);
 }
---
+++ src/game/prefabs/spawner.rs
@@ -0,0 +1,44 @@
+use std::time::Duration;
+
+use crate::game::asset_tracking::LoadResource;
+use bevy::{color::palettes::css::RED, prelude::*};
+use bevy_auto_plugin::auto_plugin::*;
+
+use super::enemy::Enemy;
+
+#[auto_register_type]
+#[auto_name]
+#[derive(Component, Debug, Copy, Clone, Reflect)]
+#[reflect(Component)]
+#[require(Transform)]
+pub struct Spawner {
+    /// What enemy will get spawned.
+    pub spawns: Enemy,
+    /// How long it takes to spawn an enemy.
+    pub spawn_duration: Duration,
+    /// Countdown for next spawn.
+    pub time_to_next_spawn: Duration,
+    /// Number of entities this spawner will create.
+    pub spawn_left: u32,
+}
+
+#[auto_plugin(app=app)]
+pub(crate) fn plugin(app: &mut App) {
+    app.add_observer(on_spawner_added);
+}
+
+fn on_spawner_added(
+    trigger: Trigger<OnAdd, Spawner>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.entity(trigger.target()).insert((
+        Mesh3d(meshes.add(Cylinder::new(4.0, 10.0))),
+        MeshMaterial3d(materials.add(StandardMaterial {
+            base_color: Color::from(RED),
+            perceptual_roughness: 1.0,
+            ..Default::default()
+        })),
+    ));
+}
