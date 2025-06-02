72f07eaf2e18736c4d275a349c8921d4400e3a09
Michail Denchev <mdenchev@users.noreply.github.com>
2025-06-02 11:23:06 -0400

    Add MovementSpeed component (#5)

--- src/game/tower_defense/behaviors/mod.rs
+++ src/game/tower_defense/behaviors/mod.rs
@@ -3,6 +3,11 @@ pub mod target_ent;
 use bevy::prelude::*;
 use bevy_auto_plugin::auto_plugin::*;
 
+#[auto_register_type]
+#[derive(Component, Debug, Copy, Clone, Reflect)]
+#[reflect(Component)]
+pub struct MovementSpeed(pub f32);
+
 #[auto_plugin(app=app)]
 pub(crate) fn plugin(app: &mut App) {
     app.add_plugins(target_ent::plugin);
