a641b219cd4aa1e97b49fa8f5f811ad99f9362da
bstriker <strikeforcezero@gmail.com>
2025-06-08 15:26:34 -0400

    chore(build): make wasm build script use --bin game

--- build-wasm.sh
+++ build-wasm.sh
@@ -2,5 +2,4 @@
 
 # cargo install --locked --git https://github.com/TheBevyFlock/bevy_cli bevy_cli
 # env -u RUSTFLAGS cargo install wasm-bindgen-cli --version 0.2.100
-RUSTFLAGS='--cfg getrandom_backend="wasm_js"' bevy build --release --no-default-features true --features wasm --yes web --bundle
-  
+RUSTFLAGS='--cfg getrandom_backend="wasm_js"' bevy build --bin game --release --no-default-features true --features wasm --yes web --bundle
